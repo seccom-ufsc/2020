8837768f9ab4a724240c72d92216a684e1dc9b49
João Vitor Maia <joaocampo2102@gmail.com>
2020-08-17 16:33:52 -0300

    Footer canvas experiment

---
+++ website/src/components/PulsarRadioBackground.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="pulsar-radio-canvas"></div>
+</template>
+
+<script>
+/* eslint-disable func-call-spacing,no-undef,no-unmodified-loop-condition */
+
+import Vue from 'vue'
+import p5 from 'p5'
+
+export default Vue.extend({
+  data () {
+    return {
+      sketch: null,
+      instance: null
+    }
+  },
+  mounted () {
+    this.sketch = function (p) {
+      let f = 0
+
+      p.setup = function () {
+        const canvas = p.createCanvas(window.innerWidth, window.innerHeight * 0.2)
+        document.querySelector('.pulsar-radio-canvas').appendChild(canvas.elt)
+        p.frameRate(60)
+      }
+
+      p.draw = function () {
+        f++
+        
+        p.background('#121212')
+        p.fill('#121212')
+        p.stroke(255) 
+
+        for (let y = 100; y < p.height; y += 5) {
+          p.beginShape()
+          let x = 0
+          for (x = 0; x < p.width; ++x) {
+            p.vertex(x, y - 80 / (1 + p.pow (x - p.width / 2, 4) / 8e6) * p.noise(x / 30 + f / 50 + y))
+          }
+          p.vertex(x, 1e4)
+          p.endShape()
+        }
+      }
+    }
+
+    this.instance = new p5(this.sketch)
+  }
+})
+</script>
+
+<style lang="scss">
+  .pulsar-radio-canvas {
+    padding: 0;
+    margin: 0;
+    // z-index: -3;
+  }
+</style>
