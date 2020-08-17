6595540db9ca67646f25636209c48df7c0e49be1
João Vitor Maia <joaocampo2102@gmail.com>
2020-08-17 16:32:25 -0300

    Implementing Contact page

--- website/src/components/ContactPage.vue
+++ website/src/components/ContactPage.vue
@@ -1,14 +1,32 @@
 <template>
-  <v-container id="contact-page" style="height: 100vh"></v-container>
+  <v-container id="contact-page">
+    <v-row class="d-flex justify-center my-15 roboto-mono">
+      <div class="text-h2" v-text="'Quer falar com a gente?'"></div>
+    </v-row>
+    <v-row class="d-flex justify-center">
+      <ContactCard class="ma-5" :contact="item" v-for="(item, index) in contacts" :key="index"/>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import ContactCard from './ContactCard'
+
 export default {
   name: 'ContactPage',
-  components: {},
+  components: {
+    ContactCard
+  },
   data: () => ({
     container: document.querySelector('#contact-page'),
-    
+    contacts: [
+      { icon: 'mdi-twitter', name: '@seccom-ufsc' },
+      { icon: 'mdi-instagram', name: '@seccom-ufsc' },
+      { icon: 'mdi-email', name: 'seccom.ufsc@gmail.com' },
+      { icon: 'mdi-github', name: 'seccom.ufsc@gmail.com' },
+      { icon: 'mdi-discord', name: 'seccom.ufsc@gmail.com' },
+      { icon: 'mdi-telegram', name: 'seccom.ufsc@gmail.com' },
+    ]
   }),
   mounted () {
     this.container = document.querySelector('#contact-page')
